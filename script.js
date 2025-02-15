cf964d5623e2c381d8223bc8ac96ffdccdc707ab
Sowparnikaaa14 <sowparnika1408@gmail.com>
2025-02-15 13:09:52 +0530

    changes in code

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const reasonsList = [
     "YOU'RE MY POOKIE BEAR",
     "you're my vaava",
+    "My Only ONE!"
+    "My man in the dreams"
     "the one who is in my maula mere maula imagination", 
     "I love the way you look at me.",
+    "My MAHARAJA",
     "You’re the most beautiful person I’ve ever met.",
     "My Favorite Place Is Inside Your Hug.",
     "You make me laugh like no one else.",
     "Your smile brightens my day.",
+    "In a world of boys, he's a gentleman.",
     "You always know how to make me feel special.",
     "You support me in everything I do.",
     "Your kindness inspires me.",
@@ -14,6 +18,7 @@ const reasonsList = [
     "You make me feel loved and appreciated.",
     "You’re my best friend and my soulmate.",
     "You’re always there for me when I need you.",
+    "My lana del rey's muse"
     "You make even the ordinary moments extraordinary.",
     "You’re incredibly thoughtful and caring.",
     "You challenge me to be a better person.",
@@ -24,6 +29,7 @@ const reasonsList = [
     "You make me feel like I’m home.",
     "You’re my everything.",
     "You’re my sunshine on a cloudy day.",
+    "you're my kalipans kanthari",
     "You’re the reason I believe in love.",
   ];
   
